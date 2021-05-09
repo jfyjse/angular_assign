65357e5a80cae76f23be075543758c7a3a97c9bf
jfyjse <joffy.jose.jfy@gmail.com>
2021-05-10 01:10:17 +0530

    inittttt....firsttt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { CreditComponent } from './credit/credit.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component : ProductsComponent
+  },
+  {
+    path: 'login',
+    component:LoginComponent
+  },
+  {
+    path: 'register',
+    component:RegisterComponent
+  },
+  {
+    path: 'cart',
+    component:CartComponent
+  },
+  {
+    path:'dummytest',
+    component:CreditComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
