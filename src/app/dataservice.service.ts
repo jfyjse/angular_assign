738ce7af1a1a2538fe732949f2f073bf993c0f13
jfyjse <joffy.jose.jfy@gmail.com>
2021-05-10 13:45:30 +0530

    logout,name printon login

--- src/app/dataservice.service.ts
+++ src/app/dataservice.service.ts
@@ -6,38 +6,46 @@ import { Router } from '@angular/router';
 })
 export class DataserviceService {
 
-  constructor(private rout:Router) { }
+  constructor(private rout: Router) { }
+
+  unme: any = 'guest'
 
 
 
-  AccountDetails=
-[
-    
-  { uname: "kannappi", emaill: "1@2.com", pwd: "111"  },
-  { uname: "luttappi", emaill: "1@3.com", pwd: "222"  },
-  { uname: "kattappa", emaill: "1@4.com", pwd: "333"  },
-  { uname: "kuttappi", emaill: "1@5.com", pwd: "444"  }
+  AccountDetails =
+    [
 
-]
+      { uname: "kannappi", emaill: "1@2.com", pwd: "111" },
+      { uname: "luttappi", emaill: "1@3.com", pwd: "222" },
+      { uname: "kattappa", emaill: "1@4.com", pwd: "333" },
+      { uname: "kuttappi", emaill: "1@5.com", pwd: "444" }
+
+    ]
+
+  name: any
+  log_status = true
+
+  login(eml: any, pwd: any) {
+
+    let emailll = this.AccountDetails.find(o => o.emaill == eml)
+    let pswd = this.AccountDetails.find(o => o.pwd == pwd)
+    this.name = this.AccountDetails.filter(item => item.emaill === eml).map(item => item.uname)
 
 
+    if (emailll && pswd) {
 
-login(eml:any,pwd:any)
-{
- 
-  let emailll= this.AccountDetails.find(o=>o.emaill==eml)
-  let pswd=this.AccountDetails.find(o=>o.pwd==pwd)
-  let nmme=this.AccountDetails.find(o=>o.uname)
-  if(emailll&&pswd)
-  {
-    alert('1')
-    this.rout.navigateByUrl('')
+      alert('sucess')
+
+      this.log_status = false
+      localStorage.setItem('name1', this.name)
+      this.unme = localStorage.getItem('name1')
+      this.rout.navigateByUrl('')
+    }
+    else {
+      alert(emailll)
+      alert("invalid details")
+    }
+
   }
-  else
-  {
-    alert("0")
-  }
-
-}
 
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { DataserviceService } from '../dataservice.service';
 
 @Component({
@@ -8,12 +9,25 @@ import { DataserviceService } from '../dataservice.service';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor(private dataasr:DataserviceService) { }
+  constructor(private dataasr:DataserviceService, private rout:Router ) { }
 
   ngOnInit(): void {
   }
+  name2=this.dataasr.unme
+  
+  
+  // name2=localStorage.getItem('name1')
 
-nme=this.dataasr.AccountDetails
+  
+  
+  log_Status=this.dataasr.log_status
+ logout(){
+   alert('click works')
+   window.localStorage.clear();
+   this.rout.navigateByUrl("login")
+
+  console.log(this.name2);
+ }
 
 
 
